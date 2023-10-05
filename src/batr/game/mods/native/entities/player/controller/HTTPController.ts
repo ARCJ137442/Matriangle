8e17e2274c7a11d5d50b4c88363b7e4489fca9e8
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2023-10-05 23:41:39 +0800

    feat: :sparkles: HTTP控制💪
    ①新增「HTTP控制器」，完全实现通过HTTP请求控制玩家②新增示例HTML，用于直接发送请求到HTTP控制器

---
+++ src/batr/game/mods/native/entities/player/controller/HTTPController.ts
@@ -0,0 +1,204 @@
+import { randInt } from "../../../../../../common/exMath";
+import { int, uint } from "../../../../../../legacy/AS3Legacy";
+import { MatrixControllerLabel } from "../../../../../api/control/MatrixControl";
+import { TPS } from "../../../../../main/GlobalGameVariables";
+import IBatrMatrix from "../../../../../main/IBatrMatrix";
+import BonusBox from "../../item/BonusBox";
+import IPlayer from "../IPlayer";
+import { ADD_ACTION, EnumPlayerAction, PlayerAction } from "./PlayerAction";
+import PlayerController from "./PlayerController";
+import { createServer, Server, IncomingMessage, ServerResponse } from 'node:http'
+import { ParsedUrlQuery, parse } from "node:querystring";
+
+/**
+ * 「HTTP控制器」
+ * * 启动一个HTTP服务器，接收外界服务器请求
+ * * 使用「控制密钥」机制，以实现「一个服务器运行，指挥多个玩家」（需要在query中提供）
+ *   * 「控制密钥」相同的玩家会被同时分派相同的动作
+ * * 请求格式：`?key=控制密钥&action=分派动作`
+ *   * 实际情况请参考类常量`KEY_CONTROL_KEY`与`KEY_ACTION`
+ * * 连接玩家时，
+ *   * 可以通过「生成订阅」直接创建链接（此时密钥=玩家名称）
+ *   * 也可以通过「添加链接」自定义「控制密钥」
+ * 
+ * ! 这个控制器需要`Node.js`支持
+ */
+export default class HTTPController extends PlayerController {
+
+	/** 共同的标签：HTTP控制器 */
+	public static readonly LABEL: MatrixControllerLabel = 'HTTP';
+
+	/** 「控制密钥」的查询键 */
+	public static readonly KEY_CONTROL_KEY: string = 'key';
+	/** 「分派动作」的查询键 */
+	public static readonly KEY_ACTION: string = 'action';
+
+	/**
+	 * 主机名称，如：
+	 * * 本地主机`localhost`
+	 * * 0.0.0.0
+	 */
+	protected _hostname: string = 'localhost';
+	public get hostname(): string { return this._hostname }
+
+	/**
+	 * 端口
+	 */
+	protected _port: uint = 8080;
+	public get port(): uint { return this._port }
+
+	/**
+	 * 存储当前HTTP服务器
+	 */
+	protected _server?: Server;
+
+	/**
+	 * 构造函数
+	 * * 不包括IP、端口的注册
+	 */
+	public constructor() {
+		super(HTTPController.LABEL);
+	}
+
+	// 服务器部分 //
+	/**
+	 * 启动HTTP服务器
+	 */
+	public launch(ip: string, port: uint) {
+		this._hostname = ip;
+		this._port = port;
+		// 创建服务器，并开始侦听
+		try {
+			this._server = createServer(this.onRequest.bind(this));
+			this._server.listen(
+				this._port, this._hostname,
+				(): void => {
+					// 启动成功
+					console.log(
+						`HTTP服务器启动成功，地址：http://${this._hostname}:${this._port}/`,
+					);
+				}
+			);
+		}
+		catch (e) {
+			console.error(`HTTP服务器${this._hostname}:${this._port}启动失败！`, e);
+		}
+	}
+
+	/**
+	 * 请求侦听函数
+	 * 
+	 * @param req 收到的请求
+	 * @param res 预备的响应
+	 */
+	public onRequest(req: IncomingMessage, res: ServerResponse): void {
+		// 解析请求
+		let queries: ParsedUrlQuery = parse(
+			req.url?.slice( // 截取出「?`a=1&b=2`...」
+				req.url.indexOf('?') + 1
+			) ?? ''
+		)
+		let controlKey: string | string[] | undefined = queries?.[HTTPController.KEY_CONTROL_KEY];
+		let action: string | string[] | undefined = queries?.[HTTPController.KEY_ACTION];
+		let responseText: string = `No response of ${req.url}\n`;
+		// 根据请求分派操作 // ! 目前只有「控制密钥」与「分派动作」均为字符串时才分派
+		if (typeof controlKey === 'string' && typeof action === 'string') {
+			this.dispatchByControlKey(controlKey, action);
+			responseText = `Action { ${controlKey}: ${action} } dispatched.\n`
+		}
+		// 响应请求
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end(responseText);
+	}
+
+	// 改写：额外的「控制密钥」绑定/解绑 //
+
+	/** 自身持有的「玩家-密钥」映射表 */
+	protected readonly _playerKeyMap: Map<IPlayer, string> = new Map<IPlayer, string>();
+	/**
+	 * 添加密钥绑定
+	 * * 默认使用玩家的自定义名称
+	 */
+	protected addControlKeyBind(
+		player: IPlayer,
+		key: string = player.customName
+	): void {
+		this._playerKeyMap.set(player, key);
+	}
+
+	/**
+	 * 移除密钥绑定
+	 * * 默认使用玩家的自定义名称
+	 */
+	protected removeControlKeyBind(player: IPlayer): void {
+		this._playerKeyMap.delete(player);
+	}
+
+	// ! 额外逻辑：增删密钥绑定
+	override addSubscriber(subscriber: IPlayer): void {
+		// 无参设置绑定
+		this.addControlKeyBind(subscriber);
+		// 继续超类逻辑
+		return super.addSubscriber(subscriber);
+	}
+
+	// ! 额外逻辑：增删密钥绑定
+	override removeSubscriber(subscriber: IPlayer): boolean {
+		// 无参设置绑定
+		this.removeControlKeyBind(subscriber);
+		// 继续超类逻辑
+		return super.removeSubscriber(subscriber);
+	}
+
+	/**
+	 * 将一个玩家连接到此控制器
+	 * 
+	 * @param player 要连接到此控制器的玩家
+	 * @param controlKey 这个玩家对应的「控制密钥」（默认是玩家的自定义名称）
+	 */
+	public addConnection(player: IPlayer, controlKey: string = player.customName): void {
+		// 无参设置绑定
+		this.addControlKeyBind(player, controlKey);
+		// 继续超类逻辑
+		return super.addSubscriber(player);
+	}
+
+	/**
+	 * 将一个玩家与此控制器断开连接
+	 * 
+	 * @param player 要与此控制器断开连接的玩家
+	 */
+	public removeConnection(player: IPlayer): boolean {
+		// 无参设置绑定
+		this.removeControlKeyBind(player);
+		// 继续超类逻辑
+		return super.removeSubscriber(player);
+	}
+
+	/**
+	 * 根据「控制密钥」分派操作
+	 */
+	protected dispatchByControlKey(controlKey: string, actionStr: string): void {
+		// 解析整数行动
+		let a: int | number = parseInt(actionStr);
+		let action: int | string = isFinite(a) ? a : actionStr;
+		// 开始遍历执行
+		for (const player of this.subscribers) {
+			if (this._playerKeyMap.get(player) === controlKey) {
+				// ! 这是唯一一个添加玩家行为的独有逻辑
+				player.onReceive(ADD_ACTION, action);
+			}
+		}
+	}
+
+	// ! 对于「玩家上报的触发」：全空回应
+	public onPlayerTick(player: IPlayer, host: IBatrMatrix): void { }
+	public reactTick(self: IPlayer, host: IBatrMatrix): PlayerAction { return EnumPlayerAction.NULL }
+	public reactHurt(self: IPlayer, host: IBatrMatrix, damage: number, attacker?: IPlayer | undefined): PlayerAction { return EnumPlayerAction.NULL }
+	public reactDeath(self: IPlayer, host: IBatrMatrix, damage: number, attacker?: IPlayer | undefined): PlayerAction { return EnumPlayerAction.NULL }
+	public reactKillPlayer(self: IPlayer, host: IBatrMatrix, victim: IPlayer, damage: number): PlayerAction { return EnumPlayerAction.NULL }
+	public reactPickupBonusBox(self: IPlayer, host: IBatrMatrix, box: BonusBox): PlayerAction { return EnumPlayerAction.NULL }
+	public reactRespawn(self: IPlayer, host: IBatrMatrix): PlayerAction { return EnumPlayerAction.NULL }
+	public reactMapTransform(self: IPlayer, host: IBatrMatrix): PlayerAction { return EnumPlayerAction.NULL }
+
+}
--- test/Matrix_V1_test.ts
+++ test/Matrix_V1_test.ts
@@ -5,25 +5,36 @@ import AIControllerGenerator from "../src/batr/game/mods/native/entities/player/
 import { NativeAIPrograms } from "../src/batr/game/mods/native/entities/player/controller/ai/NativeAIPrograms";
 import MapStorageSparse from "../src/batr/game/mods/native/maps/MapStorageSparse";
 import { NativeMaps } from "../src/batr/game/mods/native/registry/MapRegistry";
-import { getRandomTeam, loadAsBackgroundRule, randomToolEnable, respawnPlayer } from "../src/batr/game/mods/native/registry/NativeMatrixMechanics";
+import { NativeToolUsageMap, addBonusBoxInRandomTypeByRule, getRandomTeam, loadAsBackgroundRule, randomToolEnable, respawnPlayer } from "../src/batr/game/mods/native/registry/NativeMatrixMechanics";
 import Registry_V1 from "../src/batr/game/mods/native/registry/Registry_V1";
 import { NativeTools } from "../src/batr/game/mods/native/registry/ToolRegistry";
 import MatrixRule_V1 from "../src/batr/game/mods/native/rule/MatrixRule_V1";
 import Matrix_V1 from "../src/batr/game/main/Matrix_V1";
-import { 列举实体, 地图可视化, 母体可视化 } from "./可视化_test";
-import { TICK_TIME_MS, TPS } from "../src/batr/game/main/GlobalGameVariables";
+import { 列举实体, 母体可视化 } from "./可视化_test";
+import { TPS } from "../src/batr/game/main/GlobalGameVariables";
+import { mergeMaps } from "../src/batr/common/utils";
+import { NativeBonusTypes } from "../src/batr/game/mods/native/registry/BonusRegistry";
+import { iPoint } from "../src/batr/common/geometricTools";
+import HTTPController from "../src/batr/game/mods/native/entities/player/controller/HTTPController";
 
 const rule = new MatrixRule_V1();
 loadAsBackgroundRule(rule);
 
-// 设置等权重的随机地图
+// 设置等权重的随机地图 // !【2023-10-05 19:45:58】不设置会「随机空数组」出错！
 for (const map of NativeMaps.ALL_NATIVE_MAPS)
 	rule.mapRandomPotentials.set(map, 1)
+// 设置等权重的随机奖励类型 // !【2023-10-05 19:45:58】不设置会「随机空数组」出错！
+for (const bt of NativeBonusTypes._ALL_AVAILABLE_TYPE)
+	rule.bonusTypePotentials.set(bt, 1)
 
 // 设置所有工具
 rule.enabledTools = NativeTools.TOOLS_AVAILABLE
 
 const registry = new Registry_V1();
+mergeMaps(
+	registry.toolUsageMap,
+	NativeToolUsageMap
+)
 
 const matrix = new Matrix_V1(
 	rule,
@@ -33,23 +44,55 @@ console.log(matrix);
 
 matrix.initByRule();
 
-let p: IPlayer, ctl: AIControllerGenerator;
+// 创建玩家
+let p: IPlayer = new Player(
+	matrix.map.storage.randomPoint,
+	0, true,
+	getRandomTeam(matrix),
+	randomToolEnable(matrix.rule)
+);
+let p2: IPlayer = new Player(
+	new iPoint(1, 1),
+	0, true,
+	getRandomTeam(matrix),
+	randomToolEnable(matrix.rule)
+);
+// 控制器
+let ctl: AIControllerGenerator = new AIControllerGenerator(
+	'first',
+	NativeAIPrograms.AIProgram_Dummy, // 传入函数而非其执行值
+);
+let ctlHTTP: HTTPController = new HTTPController();
+ctlHTTP.launch('127.0.0.1', 3001) // 启动服务器
+// 添加实体
 matrix.addEntities(
-	p = new Player(
-		matrix.map.storage.randomPoint,
-		0, true,
-		getRandomTeam(matrix),
-		randomToolEnable(matrix.rule)
-	),
-	ctl = new AIControllerGenerator(
-		'first',
-		NativeAIPrograms.AIProgram_Dummy, // 传入函数而非其执行值
-	)
+	p, p2,
+	ctl
 )
+// 添加奖励箱
+addBonusBoxInRandomTypeByRule(matrix, new iPoint(1, 2))
+// 名字
 p.customName = 'Player初号机'
+p2.customName = 'Player二号机'
+// 生命数不减少
+p.lifeNotDecay = p2.lifeNotDecay = true;
+// 武器
+p.tool = NativeTools.WEAPON_BULLET_BASIC.copy();
+p2.tool = NativeTools.WEAPON_BULLET_BASIC.copy();
+// 初号机の控制器
 ctl.AIRunSpeed = 4; // 一秒四次行动
-p.connectController(ctl)
+p.connectController(ctl);
 respawnPlayer(matrix, p);
+// 二号机の控制器
+ctlHTTP.addConnection(p2, 'p2');
+/*
+* 地址：http://127.0.0.1:3001
+* 示例@前进：http://127.0.0.1:3001/?key=p2&action=moveForward
+* 示例@开始使用工具：http://127.0.0.1:3001/?key=p2&action=startUsing
+* 示例@停止使用工具：http://127.0.0.1:3001/?key=p2&action=stopUsing
+* 示例@转向x+：http://127.0.0.1:3001/?key=p2&action=0
+* 示例@转向前进x+：http://127.0.0.1:3001/?key=p2&action=-1
+*/
 
 // 第一次测试
 (() => {
@@ -83,24 +126,29 @@ function sleep(ms: number): Promise<void> {
 }
 
 // 预先测试：避免「异步报错无法溯源」的问题
-matrix.map = NativeMaps.EMPTY
-for (let i: uint = 0; i < TPS * 100; i++) matrix.tick();
+// matrix.map = NativeMaps.EMPTY;
+matrix.map = NativeMaps.FRAME;
+// for (let i: uint = 0; i < TPS * 10000; i++) matrix.tick();
+
+function 迭代(display_delay_ms: uint = 1000): void {
+	// TPS次迭代
+	for (let i: uint = 0; i < TPS * display_delay_ms / 1000; i++) {
+		matrix.tick();
+	}
+	// 可视化
+	console.log(
+		母体可视化(
+			matrix.map.storage as MapStorageSparse,
+			matrix.entities,
+			6
+		),
+	);
+	列举实体(matrix.entities, 5); // !【2023-10-05 17:51:21】实体一多就麻烦
+}
 
 async function 持续测试(i: int = 0, display_delay_ms: uint = 1000) {
 	for (let t = i; t !== 0; t--) {
-		// TPS次迭代
-		for (let i: uint = 0; i < TPS * display_delay_ms / 1000; i++) {
-			matrix.tick();
-		}
-		// 可视化
-		console.log(
-			母体可视化(
-				matrix.map.storage as MapStorageSparse,
-				matrix.entities,
-				1
-			),
-		);
-		列举实体(matrix.entities);
+		迭代(display_delay_ms);
 		// 延时
 		await sleep(display_delay_ms);
 	}
