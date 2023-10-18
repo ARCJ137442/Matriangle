f0b98c4f1d728b84edc667f6f42130a782c3ac58
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2023-10-18 20:54:18 +0800

    refactor(实例/客户端): :truck: 更名：client「客户端」⇒instance「实例」

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-	entry: './src/client/V1/MatriangleClient_V1.ts',
+	// 入口文件 // !【2023-10-18 12:35:07】目前是服务端
+	entry: './src/instance/V1/MatriangleServer_V1.ts',
+	// 开发者工具：内联源码映射
 	devtool: 'inline-source-map',
+	// 附加插件
 	module: {
 		rules: [
 			{
@@ -19,15 +22,18 @@ module.exports = {
 			},
 		],
 	},
+	// 需要打包的文件
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
 	},
+	// 输出
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
 	// 参考：<https://juejin.cn/post/7223644725835644989>
 	externals: [nodeExternals({
+		// 内部包仍然需要打包进最终版里
 		allowlist: [/^matriangle.*/],
 	})],
 }
